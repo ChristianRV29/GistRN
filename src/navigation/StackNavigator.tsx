bb11e3c77fd8a4948c6718a2456f40a5b29e53eb
ChristianRV29 <kmylorv@gmail.com>
2022-07-11 10:01:52 -0500

    Consume themeContext and assign themeProvider

--- src/navigation/StackNavigator.tsx
+++ src/navigation/StackNavigator.tsx
@@ -1,18 +1,24 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
 
 import { HomeScreen } from '~src/screens/HomeScreen';
+import { ThemeContext } from '~src/context/theme/theme';
 
 export type RootStackParamList = { HomeScreen: undefined };
 
 const { Navigator, Screen } = createNativeStackNavigator<RootStackParamList>();
 
 export const StackNavigator = () => {
+  const { theme } = useContext(ThemeContext);
+
   return (
-    <Navigator
-      initialRouteName="HomeScreen"
-      screenOptions={{ headerShown: false }}>
-      <Screen name="HomeScreen" component={HomeScreen} />
-    </Navigator>
+    <NavigationContainer theme={theme}>
+      <Navigator
+        initialRouteName="HomeScreen"
+        screenOptions={{ headerShown: false }}>
+        <Screen name="HomeScreen" component={HomeScreen} />
+      </Navigator>
+    </NavigationContainer>
   );
 };
