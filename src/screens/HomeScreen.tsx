b673728750af0804226022cec7a922a1022c2bc8
ChristianRV29 <kmylorv@gmail.com>
2022-07-12 07:39:51 -0500

    Add some styles to HomeScreen

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,10 +1,25 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { useContext } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+
+import { ThemeContext } from '~src/context/theme/theme';
+import { globalStyles } from '~src/styles';
 
 export const HomeScreen = () => {
+  const { theme } = useContext(ThemeContext);
+  const { top } = useSafeAreaInsets();
+
+  const { colors } = theme;
+
   return (
-    <View>
-      <Text>Welcome to homescreen</Text>
+    <View style={{ ...globalStyles.mainWrapper, top: top + 20 }}>
+      <Text style={{ ...styles.textTitle, color: colors.text }}>All gists</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  textTitle: {
+    fontSize: 35,
+  },
+});
