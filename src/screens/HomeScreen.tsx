f30d30d3bd70b5f17e4089bdc86d611234738aca
ChristianRV29 <kmylorv@gmail.com>
2022-07-12 08:03:53 -0500

    Install vector-icons for React-native

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 import { ThemeContext } from '~src/context/theme/theme';
 import { globalStyles } from '~src/styles';
@@ -12,14 +13,24 @@ export const HomeScreen = () => {
   const { colors } = theme;
 
   return (
-    <View style={{ ...globalStyles.mainWrapper, top: top + 20 }}>
+    <View
+      style={{
+        ...globalStyles.mainWrapper,
+        top: top + 20,
+        ...styles.homeWrapper,
+      }}>
       <Text style={{ ...styles.textTitle, color: colors.text }}>All gists</Text>
+      <Icon size={30} color={colors.text} name="git-branch-outline" />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
+  homeWrapper: {
+    flexDirection: 'row',
+  },
   textTitle: {
+    marginRight: 10,
     fontSize: 35,
   },
 });
